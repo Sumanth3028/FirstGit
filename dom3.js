96b9e7875f6ddea75305ffc235be79d1c859bfc2
Sumanth3028 <sumanthakula05@gmail.com>
2023-01-11 16:12:38 +0530

    third dom added

---
+++ dom3.js
@@ -0,0 +1,17 @@
+// var items=document.getElementsByClassName('list-group-item');
+// console.log(items);
+// items[2].style.background='#008000';
+// for(var i=0;i<items.length;i++)
+//  {
+//     items[i].style.fontWeight='bold';
+//  }
+
+
+ //elements by tagname
+ var li=document.getElementsByTagName('li');
+console.log(li);
+li[2].style.background='#008000';
+for(var i=0;i<li.length;i++)
+ {
+    li[i].style.fontWeight='bold';
+ }
