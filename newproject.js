023159979736d3a6e8ef5004152bf56b5b1ec0bf
Sumanth3028 <sumanthakula05@gmail.com>
2023-01-12 19:48:42 +0530

    new feature added

--- newproject.js
+++ newproject.js
@@ -2,13 +2,16 @@ var form=document.getElementById('my-form');
 var name=document.getElementById('name');
 var email=document.getElementById('email');
 form.addEventListener('submit',onSubmit);
+    let objects=[];
     function onSubmit(e){
         e.preventDefault();
-        var nameVal=e.target.name.value;
+        var name=e.target.name.value;
+        var email=e.target.email.value;
+
+        var Myobj={name,email};
+        objects.push(Myobj);
+        localStorage.setItem('obj',JSON.stringify(objects));
+        console.log(JSON.parse(JSON.stringify(objects)));
+       
         
-        var emailVal=e.target.email.value;
-  
-        localStorage.setItem('name',nameVal);
-        localStorage.setItem('email',emailVal);
-        console.log(localStorage.getItem('name'));
     }
